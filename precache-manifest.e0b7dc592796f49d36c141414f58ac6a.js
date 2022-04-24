4ca08ae88c165686f8fea9792ddeefe2c72b20b9
Rishabhltfb <Rishabhltfb@users.noreply.github.com>
2022-04-24 06:41:14 +0000

    deploy: 1c9e76bb8bf0b829de6a7ca6d7738b8e95d8dea6

---
+++ precache-manifest.e0b7dc592796f49d36c141414f58ac6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d06179226cae03e21457440eb0dbf15",
+    "url": "https://Rishabhltfb.github.io/react-frontend/index.html"
+  },
+  {
+    "revision": "47751669481e830be73e",
+    "url": "https://Rishabhltfb.github.io/react-frontend/static/css/main.2c6b28d2.chunk.css"
+  },
+  {
+    "revision": "bee1e037169154246216",
+    "url": "https://Rishabhltfb.github.io/react-frontend/static/js/2.20997c69.chunk.js"
+  },
+  {
+    "revision": "47751669481e830be73e",
+    "url": "https://Rishabhltfb.github.io/react-frontend/static/js/main.5f00933e.chunk.js"
+  },
+  {
+    "revision": "87c75eaee4c79e5682a1",
+    "url": "https://Rishabhltfb.github.io/react-frontend/static/js/runtime~main.5a14c3ac.js"
+  }
+]);
